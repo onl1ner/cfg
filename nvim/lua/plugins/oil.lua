10aa11ef61ed7113078d3fd87ac67cb7d0a1b446
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2024-06-23 23:28:35 +0500

    Added oil plugin, removed tree

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require('oil').setup {
+            view_options = {
+                show_hidden = true,
+            },
+        }
+
+        vim.keymap.set("n", "<leader>ee", "<cmd>Oil<cr>")
+    end
+}
