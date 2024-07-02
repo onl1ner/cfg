951cfa8e6f16021c19178893bb58cc66b715380f
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2024-07-02 19:30:54 +0500

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
-brew install koekeishiya/formulae/yabai
-brew install koekeishiya/formulae/skhd
-
-brew install nvim tmux lazygit
-brew install xcbeautify xcode-build-server
-
 mkdir -p ~/.config
+rm -rf ~/.config/alacritty ~/.config/nvim
 
-cp -R -f alacritty ~/.config
-cp -R -f nvim ~/.config
+cp -R alacritty ~/.config
+cp -R nvim ~/.config
 
 cp zsh/.zshrc ~/.zshrc
 
-cp -R -f yabai ~/.config && cp -R -f skhd ~/.config
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	brew install koekeishiya/formulae/yabai
+	brew install koekeishiya/formulae/skhd
+
+	brew install nvim tmux lazygit
+	brew install xcbeautify xcode-build-server
+
+	rm -rf ~/.config/yabai ~/.config/skhd
+	cp -R yabai ~/.config && cp -R skhd ~/.config
+fi
