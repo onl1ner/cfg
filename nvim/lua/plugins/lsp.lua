6978cb08be061502a52db91f11de039a15a4c416
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2024-07-06 20:51:19 +0500

    Added godot support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -76,5 +76,10 @@ return {
                 },
             }
         }
+
+        lspconfig.gdscript.setup{
+            name = "godot",
+            cmd = vim.lsp.rpc.connect("127.0.0.1", "6005")
+        }
     end,
 }
